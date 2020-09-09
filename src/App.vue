e829b9430ce9da1d35c64c033685d02603c15dcb
Muhammad Ajmal Aumeeruddy <BlackBanner@users.noreply.github.com>
2020-09-09 12:07:51 +0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <v-app id="inspire">
       <v-toolbar color="indigo" dark fixed app>
-        <v-toolbar-title>Form</v-toolbar-title>
+        <v-toolbar-title>aut-num Form</v-toolbar-title>
         <v-spacer></v-spacer>
       </v-toolbar>
 
@@ -20,7 +20,7 @@
                 >
                   <ValidationProvider
                     name="aut_num"
-                    :rules="{ required: true}"
+                    :rules="{ required:true, unique:true}"
                     v-slot="{ errors }"
                   >
                     <v-text-field
@@ -36,7 +36,7 @@
 
                   <ValidationProvider
                     name="as_name"
-                    :rules="{ required: false}"
+                    :rules="{ required: true}"
                     v-slot="{ errors }"
                   >
                     <v-text-field
@@ -52,7 +52,7 @@
 
                   <ValidationProvider
                     name="descr"
-                    :rules="{ required: false}"
+                    :rules="{ required: true}"
                     v-slot="{ errors }"
                   >
                     <v-text-field
@@ -61,6 +61,21 @@
                       class="custom_label custom_input"
                       placeholder=" "
                       v-model="descr"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+                  <ValidationProvider
+                    name="status"
+                    :rules="{ required: true}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="status"
+                      name="status"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="status"
                       :error-messages="errors"
                     >
                     </v-text-field>
@@ -76,7 +91,7 @@
                       name="member_of"
                       class="custom_label custom_input"
                       placeholder=" "
-                      v-model="descr"
+                      v-model="member_of"
                       :error-messages="errors"
                     >
                     </v-text-field>
@@ -109,6 +124,246 @@
                       class="custom_label custom_input"
                       placeholder=" "
                       v-model="mp_import"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="export"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="export"
+                      name="export"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="exports"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="mp_export"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="mp-export"
+                      name="mp_export"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="mp_export"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="default"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="default"
+                      name="default"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="defaults"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="mp_default"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="mp-default"
+                      name="mp_default"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="mp_default"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="remarks"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="remarks"
+                      name="remarks"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="remarks"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="org"
+                    :rules="{ required: true}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="org"
+                      name="org"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="org"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="admin_c"
+                    :rules="{ required: true}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="admin-c"
+                      name="admin_c"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="admin_c"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+                  
+                  <ValidationProvider
+                    name="tech_c"
+                    :rules="{ required: true}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="tech-c"
+                      name="tech_c"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="tech_c"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+                  
+                  <ValidationProvider
+                    name="notify"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="notify"
+                      name="notify"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="notify"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="mnt_irt"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="mnt-irt"
+                      name="mnt_irt"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="mnt_irt"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="mnt_lower"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="mnt-lower"
+                      name="mnt_lower"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="mnt_lower"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="mnt_routes"
+                    :rules="{ required: false}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="mnt-routes"
+                      name="mnt_routes"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="mnt_routes"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="mnt_by"
+                    :rules="{ required: true}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="mnt-by"
+                      name="mnt_by"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="mnt_by"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="change"
+                    :rules="{ required: true}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="change"
+                      name="change"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="change"
+                      :error-messages="errors"
+                    >
+                    </v-text-field>
+                  </ValidationProvider>
+
+                  <ValidationProvider
+                    name="source"
+                    :rules="{ required: true}"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      label="source"
+                      name="source"
+                      class="custom_label custom_input"
+                      placeholder=" "
+                      v-model="source"
                       :error-messages="errors"
                     >
                     </v-text-field>
@@ -151,9 +406,25 @@ export default {
       aut_num:'',
       as_name:'',
       descr:'',
+      status:'',
       member_of:'',
       imports:'',
-      mp_import:''
+      mp_import:'',
+      exports:'',
+      mp_export:'',
+      defaults:'',
+      mp_default:'',
+      remarks:'',
+      org:'',
+      admin_c:'',
+      tech_c:'',
+      notify:'',
+      mnt_irt:'',
+      mnt_lower:'',
+      mnt_routes:'',
+      mnt_by:'',
+      change:'',
+      source:''
     };
   },
 
@@ -173,11 +444,32 @@ export default {
             required: "Aut-num is required",
           },
           as_name: {
-            required: "Aut-num is required",
+            required: "as_name is required",
           },
           descr: {
-            required: "Aut-num is required",
+            required: "descr is required",
           },
+          status: {
+            required: "status is required",
+          },
+          org: {
+            required: "org is required",
+          },
+          admin_c: {
+            required: "admin_c is required",
+          },
+          tech_c: {
+            required: "tech_c is required",
+          },
+          mnt_by: {
+            required: "mnt_by is required",
+          },
+          change: {
+            required: "change is required",
+          },
+          source: {
+            required: "source is required",
+          }
         },
       },
     });
@@ -190,7 +482,7 @@ export default {
 };
 </script>
 <style>
-.custom_input{
+  .custom_input{
 
-}
+  }
 </style>
